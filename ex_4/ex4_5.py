424cbbe8362c143a467dff4fb8be390f531bdb91
DenisArger <den.arger@gmail.com>
2022-10-11 09:53:13 +0300

    ex_4_5 done

---
+++ ex_4/ex4_5.py
@@ -0,0 +1,55 @@
+# 5 Даны два файла, в каждом из которых находится запись многочлена.
+#  Задача - сформировать файл, содержащий сумму многочленов.
+# Коэффициенты могут быть как положительными, так и отрицательными.
+#  Степени многочленов могут отличаться.
+
+
+with open("./first_file.txt", "r") as file:
+    first_file = file.read()
+with open("./second_file.txt", "r") as file:
+    second_file = file.read()
+
+
+def to_tuple(my_string):
+    my_string = str.replace(my_string, "- ", "+ -").split()
+    my_string = list(filter(lambda x: x != "+" and x != "=" and x != "0", my_string))
+    for i in range(len(my_string)):
+        my_string[i] = my_string[i].split("*")
+    return my_string
+
+
+first_file = to_tuple(first_file)
+second_file = to_tuple(second_file)
+
+sum = first_file + second_file
+for i in range(len(sum)):
+    if len(sum[i]) == 1:
+        sum[i] = [sum[i].pop(0), "X^0"]
+cash = []
+answer = []
+
+print(sum)
+
+for i in range(len(sum)):
+    key = sum[i][1]
+    if key not in cash:
+        cash.append(key)
+        current_sum = int(sum[i][0])
+        for j in range(i + 1, len(sum)):
+            if key == sum[j][1]:
+                current_sum += int(sum[j][0])
+        answer.append([current_sum, key])
+
+
+for i in range(len(answer)):
+    answer[i] = str(answer[i][0]) + "*" + str(answer[i][1])
+answer = str(" + ".join(answer))
+
+answer = str.replace(answer, "+ -", "- ")
+answer = str.replace(answer, "*X^0", "")
+answer = str.replace(answer, "1*", "")
+
+answer += " = 0"
+
+with open("ready_ex5.txt", "w") as file:
+    file.write(answer)
