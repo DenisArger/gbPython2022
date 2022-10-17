668a2b10cc2db439236af283b24741cb38785ac9
DenisArger <den.arger@gmail.com>
2022-10-17 19:36:26 +0300

    ex2_1 update

--- ex_2/ex2_1.py
+++ ex_2/ex2_1.py
@@ -7,15 +7,25 @@
 # 0,56 -> 11
 
 
-def sum_number():
+from unittest import result
+
+# Было
+# def sum_number():
+#     print("Введите число")
+#     number = float(input())
+
+#     sum = 0
+#     for i in str(number):
+#         if i != ".":
+#             sum += int(i)
+#     print(f"Сумма цифр равна: {(sum)}")
+
+# Стало
+def sum_of_digits_new():
     print("Введите число")
-    number = float(input())
-
-    sum = 0
-    for i in str(number):
-        if i != ".":
-            sum += int(i)
-    print(f"Расстояние между двумя точками равно: {(sum)}")
+    number = input()
+    result = sum([int(number[i]) for i in range(len(number)) if number[i].isdigit()])
+    print(f"Сумма цифр равна: {(result)}")
 
 
-sum_number()
+sum_of_digits_new()
