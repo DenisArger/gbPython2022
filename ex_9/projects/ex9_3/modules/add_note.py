25ee1fd9c7d7d159dc83863020db0db443e09a3c
DenisArger <den.arger@gmail.com>
2022-11-11 10:27:15 +0300

    ex9_3 done

---
+++ ex_9/projects/ex9_3/modules/add_note.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def add_note(family=None, name=None, tel=None, discription=None):
+    with open("database.csv", "a", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["Фамилия", "Имя", "Телефон", "Описание"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(
+            {"Фамилия": family, "Имя": name, "Телефон": tel, "Описание": discription}
+        )
